669a00dfc332a1ba65f6d7b965907d87913d6a30
Maria Saari <maria.saari@outlook.coom>
2023-04-18 12:09:03 +0300

    create_event ok

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # import string
+from typing import List
+from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -22,7 +24,7 @@ class EventsBase(BaseModel):
 # #2
 class PlayerDb(PlayerIn):
      id: int
-     #events: (list[EventsBase])
+     events: List[EventsBase] = []
 
 # #5
 class EventsDb(EventsBase):
@@ -39,18 +41,21 @@ players = [
 
 events = [
     {'id':0, 'type':'level_started', 'detail': 'v2021', 'player_id':1, 'timestamp':'12.4.2023 19:28'},
+    {'id':4, 'type':'level_solved', 'detail': 'v2021', 'player_id':1, 'timestamp':'12.4.2023 19:28'},
     {'id':1, 'type':'level_solved', 'detail': 'c2020', 'player_id':2, 'timestamp':'12.4.2023 19:28'},
     {'id':2, 'type':'level_started', 'detail': '98713f', 'player_id':3, 'timestamp':'12.4.2023 19:28'}, 
     {'id':3, 'type':'level_solved', 'detail': 'f2335', 'player_id':4, 'timestamp':'12.4.2023 19:28'} 
 ]
 
 #GET /players - palauttaa pelaajien nimet ja id:t
+#laita ensin id ja sitten nimi
 @app.get('/players')
 def player_name_id():
     player_names_and_ids = [{"id": player["id"], "name": player["name"]} for player in players]
     return player_names_and_ids
 
 #GET /players/{id} - palauttaa tietyn pelaajan kaikki tiedot
+#timestamp puuttuu
 @app.get("/players/{id}")
 def get_player(id: int):
     for player in players:
@@ -71,13 +76,45 @@ def create_player(player_in:PlayerIn):
 
 #4. GET /players/{id}/events - palauttaa tietyn pelaajan kaikki eventit
 @app.get('/players/{id}/events')
-def get_player_events(id: int, type: str):
+def get_player_events(id: int, type: str = None):
     #tarkista pelaaja
-    for i in range(len(players)):
-        if players[i] == id:
-            player = players[i]
-        else:
-            raise HTTPException(detail="player not found", status_code=404)
+    player = None
+    for p in players:
+        if p['id'] == id:
+            player = p
+            break
+    if not player:
+        raise HTTPException(status_code=404, detail='Player not found')
+
     #tarkista event
-    player_events = [event for event in events if event['player_id'] == id and event['event_type'] in ['level_started', 'level_sorted']]
+    if type is not None:
+        if type not in ['level_started', 'level_sorted']:
+            raise HTTPException(status_code=400, detail='Invalid event')
+
+        player_events = [event for event in events if event['player_id'] == id and event['type'] == type]
+    else:
+        player_events = [event for event in events if event['player_id'] == id]
+
     return player_events
+
+#5. POST /players/{id}/events - luo uuden eventin pelaajalle
+#tuplakoodia! korjaa! (pelaajan ja eventin tarkistus)
+@app.post('/players/{id}/events', status_code=201, response_model=EventsDb,)
+def create_event(id: int, event: EventsBase, type: str = None):
+    #tarkista pelaaja
+    player = None
+    for p in players:
+        if p['id'] == id:
+            player = PlayerDb(**p)
+            break
+    if not player:
+        raise HTTPException(status_code=404, detail='Player not found',)
+    #tarkista event type
+    if type is not None:
+        if type not in ['level_started', 'level_sorted']:
+            raise HTTPException(status_code=400, detail='Invalid event')
+    #luo uusi event
+    new_event = EventsDb(**event.dict(), player_id=id, timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    player.events.append(new_event)
+    return new_event
+
