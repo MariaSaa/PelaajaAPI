7eb7641e95660920b92c8c73a9bb201a3095b55a
Maria Saari <maria.saari@outlook.coom>
2023-04-28 15:56:09 +0300

    refactor

---
+++ app/database/database.py
@@ -0,0 +1,16 @@
+players = [
+    {'id':1, 'name':'Maria'},
+    {'id':2, 'name':'Reijo'},
+    {'id':3, 'name':'Veijo'},
+    {'id':4, 'name':'Keijo'}
+]
+
+events = [
+    {'id':0, 'type':'level_started', 'detail': 'v2021', 'player_id':1, 'timestamp':'12.4.2023 19:28'},
+    {'id':4, 'type':'level_solved', 'detail': 'v2021', 'player_id':1, 'timestamp':'12.4.2023 19:28'},
+    {'id':1, 'type':'level_solved', 'detail': 'c2020', 'player_id':2, 'timestamp':'12.4.2023 19:28'},
+    {'id':2, 'type':'level_started', 'detail': '98713f', 'player_id':3, 'timestamp':'12.4.2023 19:28'}, 
+    {'id':3, 'type':'level_solved', 'detail': 'f2335', 'player_id':4, 'timestamp':'12.4.2023 19:28'} 
+]
+
+#tee tänne kaikki businesslogiikka ja poista ne endpointeista
